f49376132d1fda2a9d0b260f3e097b354d2a064e
abdelhalimlakfifi <abdelhalimlakfifi@gmail.com>
2021-11-14 17:53:21 +0100

    something

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import discord
 import asyncio
+from discord import channel
 from discord.utils import get
+from discord.ext import commands
+
 
 intents = discord.Intents.default()
 intents.members = True
 client = discord.Client(intents=intents)
+client2 = commands.Bot(command_prefix="!")
+
 
 @client.event
 async def on_ready():
@@ -18,5 +23,20 @@ async def on_member_join(member):
     await member.add_roles(role)
 
 
+@client.event
+async def on_voice_state_update(member,before, after):
+    channel = client.get_channel(904467050104254545)
+    if not before.channel and after.channel:
+        await channel.send(f'<@{member.id}>  joined **{after.channel}**')
+        # print(f'{member} has  joined {after.channel}')
+    elif not after.channel and before.channel:
+        await channel.send(f'<@{member.id}> left **{before.channel}**')
 
-client.run('OTAzOTk3OTQ4OTQ4NTQ1NTY2.YX1Hsg.MR0woOjr3wniE-qX_JeAqEkcwiw')
+
+
+@client2.command
+async def test(ctx):
+    print("test")
+
+
+client.run('OTAzOTk3OTQ4OTQ4NTQ1NTY2.YX1Hsg.e6RIzXLQduqm9OGyDCCvcDCP-vY')
